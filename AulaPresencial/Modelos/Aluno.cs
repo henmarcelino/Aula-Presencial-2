2df3afeb8c156fe46ecd240ebe8e046d718cd5f9
Aluno <Aluno@DOSO121799>
2018-07-07 08:53:21 -0300

    7/7

--- AulaPresencial/Modelos/Aluno.cs
+++ AulaPresencial/Modelos/Aluno.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Modelos
 {
+    /*Context ctx = new Context();
+        ctx.Aluno.add(novoAluno);
+        ctx.savechanges();*/
     public class Aluno
     {
         public string Nome { get; set; }
